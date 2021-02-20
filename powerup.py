0756e33d627c00ba43ce375499120a519d8f5527
princesinghtomar <singhtomarprince470@gmail.com>
2021-02-20 06:56:15 +0530

    nothing special

--- powerup.py
+++ powerup.py
@@ -9,13 +9,14 @@ import time
 # Active = 1 // released but not yet taken by the player
 # Active = 2 // taken by player
 
-class power0:
+class powerupclass:
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
         self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 0
 
     def update_time_activated(self):
         self.time_activated = time.time()
@@ -49,13 +50,13 @@ class power0:
         #self.check_time()
         if(self.active == 1):
             if(self._x < 43):
-                temp = powerup_temper[0]
+                temp = powerup_temper[self.index]
                 screen_array[self._x-1][self._y] = ' '
                 screen_array[self._x][self._y] = temp
             elif(self._x > 43):
                 self.active = 0
             else:
-                temp = powerup_temper[0]
+                temp = powerup_temper[self.index]
                 screen_array[self._x-1][self._y] = ' '
                 if(self._y >= paddle_start and self._y <= paddle_end):
                     self.active = 2
@@ -63,19 +64,30 @@ class power0:
             self._x += 1
             return False
 
+class power0(powerupclass):
+
+    def __init__(self,x,y):
+        self.time_activated = time.time()
+        self.active = 0
+        self.max_time = 15
+        self._x = x + 5
+        self._y = y
+        self.index = 0
+
     def do(self,Paddle):
         Paddle.update_type(2)
     
     def undo(self,Paddle):
         Paddle.update_type(1)
 
-class power1(power0):
+class power1(powerupclass):
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
-        self.max_time = 10
+        self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 1
 
     def do(self,Paddle):
         Paddle.update_type(0)
@@ -83,13 +95,14 @@ class power1(power0):
     def undo(self,Paddle):
         Paddle.update_type(1)
     
-class power2(power0):
+class power2(powerupclass):
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
-        self.max_time = 5
+        self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 2
 
     def do(self,ball_class,screen_array,balls,ball_index):
         x = ball_class
@@ -112,13 +125,14 @@ class power2(power0):
         # index.pop(0)
         #undo nothing now
 
-class power3(power0):
+class power3(powerupclass):
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
-        self.max_time = 5
+        self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 3
 
     def do(self,ball_class):
         # for i in ball_class:
@@ -131,13 +145,14 @@ class power3(power0):
         #     i.increase_speed(-2,-2)   for multiple balls
         ball_class.increase_speed(-2,-2)
 
-class power4(power0):
+class power4(powerupclass):
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
-        self.max_time = 5
+        self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 4
 
     def do(self,ball_class):
         ball_class.update_thru_ball(True)
@@ -145,13 +160,14 @@ class power4(power0):
     def undo(self,ball_class):
         ball_class.update_thru_ball(False)
 
-class power5(power0):
+class power5(powerupclass):
     def __init__(self,x,y):
         self.time_activated = time.time()
         self.active = 0
-        self.max_time = 5
+        self.max_time = 15
         self._x = x + 5
         self._y = y
+        self.index = 5
 
     def do(self):
         # print("yup Reaced here")
